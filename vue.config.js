7590f5b74cd7a4ae1dcc3190cc21d847c30ad616
cyh <cyh@qq.com>
2022-05-19 16:10:13 +0800

    test

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  lintOnSave: false,
+
+  pwa: {
+    name: 'test',
+    themeColor: '#4c89fe',
+    msTileColor: '#4c89fe',
+    manifestOptions: {
+      start_url: '.',
+      background_color: '#4c89fe'
+    },
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+    }
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
