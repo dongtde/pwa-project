fd229c22310a325125ceea8148c305c4715eccf2
cyh <cyh@qq.com>
2022-05-20 11:35:27 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <!-- -->
-    </v-app-bar>
-
     <!-- 根据应用组件来调整你的内容 -->
     <v-main>
       <!-- 给应用提供合适的间距 -->
@@ -18,10 +10,6 @@
         </div>
       </v-container>
     </v-main>
-
-    <v-footer app>
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
